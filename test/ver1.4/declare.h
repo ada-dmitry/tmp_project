c022a02959c715f7a6a4daea3b1f5e8646c76498
ada-dmitry <ada.dmitry@gmail.com>
2023-12-07 14:36:52 +0300

    reboot prod

--- test/ver1.4/declare.h
+++ test/ver1.4/declare.h
@@ -4,6 +4,9 @@ extern short method, *mth, sig_flag;
 // Декларирование глобальных переменных,
 // как внешних для последующего использования их во всей программе.
 #define H 0.000001
+#define A -10000
+#define B 10000
+#define EPS 0.001
 
 double f(double x);
 void ctrlc_handler(int signum);
@@ -14,3 +17,4 @@ void newton_method();
 double numerical_derivative(double x);
 void exit_777();
 void show_grapgh();
+void input_diap();
